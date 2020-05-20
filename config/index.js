0bd86786d54ed4d0ed0a9c2b72707b6f06702814
unknown <andrasimion99@yahoo.com>
2020-05-20 17:21:46 +0300

    cors

--- config/index.js
+++ config/index.js
@@ -1,6 +1,16 @@
-const port = parseInt(process.env.PORT, 10) || 3002;
+const dotenv = require('dotenv');
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (!envFound) {
+	// This error should crash whole process
+
+	throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
 module.exports = {
-  PORT: port,
+  PORT: parseInt(process.env.PORT, 10),
   MONGO_CONNECTION_STRING:
     "mongodb+srv://andra-raluca:Bisl2k5pczRc4kPK@ip-project-yau3a.mongodb.net/auth_management?authSource=admin&replicaSet=ip-project-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true",
 };
