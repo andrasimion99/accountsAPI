c0026ec6b9d237af080abdfda70d77ea551e1958
unknown <andrasimion99@yahoo.com>
2020-05-20 16:22:07 +0300

    port

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ if (!envFound) {
 	throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 module.exports = {
-  PORT: 3002 || parseInt(process.env.PORT, 10),
+  PORT: parseInt(process.env.PORT, 10),
   MONGO_CONNECTION_STRING:
     "mongodb+srv://andra-raluca:Bisl2k5pczRc4kPK@ip-project-yau3a.mongodb.net/auth_management?authSource=admin&replicaSet=ip-project-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true",
 };
--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const http = require("http");
-const dotenv = require('dotenv');
 
-// Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-const envFound = dotenv.config();
-if (!envFound) {
-	// This error should crash whole process
-
-	throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
 const { router } = require("./router");
 const { routes } = require("./router");
 const { mongoose } = require("./router");
 const { PORT } = require("./config");
-const port = PORT || parseInt(process.env.PORT, 10);
 
 process.on("uncaughtException", function (err) {
   console.log("uncaughtException");
@@ -26,8 +15,8 @@ const server = http.createServer(async (req, res) => {
   await router(req, res, routes);
 });
 
-server.listen(port, async () => {
+server.listen(PORT, async () => {
   await mongoose();
   console.log("Connected to database successfully");
-  console.log("Server is listening on port " + port);
+  console.log("Server is listening on port " + PORT);
 });
