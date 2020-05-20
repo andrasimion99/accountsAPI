f69850f202ffb5d536a9e7353dc4e03ea2c2a6f8
unknown <andrasimion99@yahoo.com>
2020-05-20 15:58:04 +0300

    port

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 3002,
+  PORT: 3002 || parseInt(process.env.PORT, 10),
   MONGO_CONNECTION_STRING:
     "mongodb+srv://andra-raluca:Bisl2k5pczRc4kPK@ip-project-yau3a.mongodb.net/auth_management?authSource=admin&replicaSet=ip-project-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true",
 };
