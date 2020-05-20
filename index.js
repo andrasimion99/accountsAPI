84fc9a1f7dec675c15cb571035e66b8300f40962
unknown <andrasimion99@yahoo.com>
2020-05-20 16:17:36 +0300

    inca o data

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const http = require("http");
+const dotenv = require('dotenv');
 
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (!envFound) {
+	// This error should crash whole process
+
+	throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
 const { router } = require("./router");
 const { routes } = require("./router");
 const { mongoose } = require("./router");
