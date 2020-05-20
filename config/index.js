f886ec6c93a7bf08a6a9fcfe46762d868efac293
unknown <andrasimion99@yahoo.com>
2020-05-20 16:15:03 +0300

    dotenv

--- config/index.js
+++ config/index.js
@@ -1,3 +1,14 @@
+const dotenv = require('dotenv');
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (!envFound) {
+	// This error should crash whole process
+
+	throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
 module.exports = {
   PORT: 3002 || parseInt(process.env.PORT, 10),
   MONGO_CONNECTION_STRING:
